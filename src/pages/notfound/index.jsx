ce1fdef9080ec747f47235e89bf73c3fbfe2d79f
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-11-06 22:16:07 +0600

    Create: NotFound component for 404 error page
    This commit introduces the 'NotFound' component.
    
    The 'NotFound' component can be used in our application to improve the user experience when encountering 404 errors.

--- src/pages/notfound/index.jsx
+++ src/pages/notfound/index.jsx
@@ -1,5 +1,21 @@
+import { images } from "../../constant";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>Not Found</div>;
+  return (
+    <div className="h-screen flex-col w-full flex justify-center items-center bg-[#f7f7f7]">
+      <img
+        src={images.notfound}
+        alt="notfound"
+        className=" h-[300px] w-[400px] md:h-[600px] md:w-[800px]"
+      />
+      <Link
+        to="/"
+        className=" text-xl font-bold text-zinc-600 cursor-pointer border-2 px-4 py-2 border-green-100 hover:text-green-500 duration-300 transition-colors">
+        Back Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
