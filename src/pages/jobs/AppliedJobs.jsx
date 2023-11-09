db8fbf860f76e1cd8a34dea6a067e00935253a0b
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-11-09 12:18:02 +0600

    Refactor AppliedJobs component with filtering and checkboxes
    This commit refactors the AppliedJobs component to include filtering based on selected categories using checkboxes. It also updates the UI to display a table with job details, including name, categories, salary, and a view link. Additionally, it includes loading skeletons for better user experience during data fetching

--- src/pages/jobs/AppliedJobs.jsx
+++ src/pages/jobs/AppliedJobs.jsx
@@ -2,18 +2,27 @@ import PageHeader from "../../components/common/PageHeader";
 import { useEffect, useState } from "react";
 import { request } from "../../utils/axios";
 import useAuthentication from "../../hooks/useAuthentication";
-import JobCardSkeleton from "../../components/skeleton/JobCardSkeleton";
-import JobCard from "../../components/common/JobCard";
-import Container from "../../components/common/Container";
 import useTitleSet from "../../hooks/useTitleSet";
-const catagories = ["All Job", "On Site", "Remote", "Hybrid", "Part Time"];
+import { Link } from "react-router-dom";
+import TableSkeleton from "../../components/skeleton/TableSkeleton";
+import CheckBoxInput from "../../components/common/ChakBoxInput";
+
+const catagories = ["On Site", "Remote", "Hybrid", "Part Time"];
+
+const bgColorByCatagories = {
+  "On Site": "bg-green-100",
+  Remote: "bg-violet-100",
+  "Part Time": "bg-orange-100",
+  Hybrid: "bg-lime-100",
+};
 
 const AppliedJobs = () => {
   const [data, setData] = useState([]);
-  const [jobs, setJobs] = useState([]);
+  const [selectCatagories, setSelectCatagories] = useState([]);
+  const [filterJobs, setFilterJobs] = useState([]);
+
   const [loading, setLoading] = useState(false);
   const { user, loading: loadingUser } = useAuthentication();
-  const [activeCatagoriesIndex, setActiveCatagoriesIndex] = useState(0);
   useTitleSet("Applied Jobs");
 
   useEffect(() => {
@@ -30,72 +39,132 @@ const AppliedJobs = () => {
       });
   }, [loadingUser]);
 
-  const handleActiveCatagories = (index) => {
-    setActiveCatagoriesIndex(index);
-    if (index === 0) {
-      setJobs(data);
-      return;
-    } else if (index > 0) {
-      const filterJobs = data?.filter(
-        (item) => item?.job.catagories === catagories[index]
-      );
-      console.log("filterJobs", filterJobs);
-      setJobs(filterJobs);
-    }
-  };
+  let jobs = filterJobs.length > 0 ? filterJobs : data;
 
-  useEffect(() => {
-    if (data?.length > 0) {
-      setJobs(data);
+  const handleCatagoriesCheck = (value) => {
+    const findSelectCatagories = selectCatagories?.find((i) => i === value);
+
+    if (findSelectCatagories) {
+      const newSelectCatagories = selectCatagories?.filter((i) => i !== value);
+      setSelectCatagories(newSelectCatagories);
+
+      const RemoveSelectedCatagoriesJobs = filterJobs?.filter(
+        (item) => item?.job.catagories !== value
+      );
+      setFilterJobs(RemoveSelectedCatagoriesJobs);
+      return;
     }
-  }, [data]);
+    const filterSelectedCatagoriesJobs = data?.filter(
+      (item) => item?.job.catagories === value
+    );
+
+    setFilterJobs((prev) => [...prev, ...filterSelectedCatagoriesJobs]);
+
+    setSelectCatagories((prev) => [...prev, value]);
+  };
 
   return (
     <div>
       <PageHeader title="Applied Jobs" />
-      <div className=" flex flex-wrap justify-center items-center gap-4 my-5">
-        {catagories.map((item, i) => {
-          const active = activeCatagoriesIndex === i;
-          return (
-            <span
-              onClick={() => handleActiveCatagories(i)}
-              className={` ${
-                active
-                  ? " bg-green-500 text-white"
-                  : "text-green-500 bg-[#22c55e32]"
-              } p-2 px-4 rounded-md  font-semibold text-md cursor-pointer `}
-              key={i}>
-              {item}
-            </span>
-          );
-        })}
-      </div>
-      <div className=" my-16">
-        <Container>
-          {loading || loadingUser ? (
-            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-8">
-              <JobCardSkeleton />
-              <JobCardSkeleton />
-              <JobCardSkeleton />
-              <JobCardSkeleton />
-              <JobCardSkeleton />
-              <JobCardSkeleton />
+
+      <section className="container px-4 mx-auto my-20">
+        <div className=" flex flex-wrap justify-center items-center  mt-3 border-[1px] border-b-0 w-fit">
+          {catagories.map((item, i) => {
+            return (
+              <span className={` px-2 md:px-3 py-2 cursor-pointer `} key={i}>
+                <CheckBoxInput
+                  label={item}
+                  onClick={() => handleCatagoriesCheck(item)}
+                />
+              </span>
+            );
+          })}
+        </div>
+        <div className="flex flex-col min-h-[400px]">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden border border-gray-200  md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200 ">
+                  <thead className="bg-gray-50 ">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-zinc-700">
+                        Name
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-zinc-700">
+                        Catagories
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-zinc-700">
+                        Salary
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-zinc-700">
+                        View
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200 ">
+                    {loading && (
+                      <>
+                        <TableSkeleton />
+                        <TableSkeleton />
+                        <TableSkeleton />
+                        <TableSkeleton />
+                      </>
+                    )}
+
+                    {jobs?.length > 0 &&
+                      jobs.map((item) => (
+                        <tr key={item._id}>
+                          <td className="px-4 py-4 text-sm text-zinc-700 whitespace-nowrap">
+                            {item.job.title}
+                          </td>
+
+                          <td className="px-4 py-4 text-sm text-zinc-700 whitespace-nowrap">
+                            <div
+                              className={`${
+                                bgColorByCatagories[item.job.catagories]
+                              } w-fit px-2 pyy-1 rounded-2xl text-sm`}>
+                              {item.job.catagories}
+                            </div>
+                          </td>
+                          <td className="px-4 py-4 text-sm text-zinc-700 whitespace-nowrap">
+                            {item.job.salary}
+                          </td>
+                          <td className="px-4 py-4 text-sm whitespace-nowrap">
+                            <div className="flex items-center gap-x-6">
+                              <Link
+                                to={`/job/${item.job._id}`}
+                                className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
+                                View
+                              </Link>
+                            </div>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
-          ) : (
-            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-8">
-              {jobs?.length > 0 &&
-                jobs.map((item) => <JobCard key={item._id} item={item.job} />)}
-            </div>
-          )}
-        </Container>
+          </div>
+        </div>
         {jobs?.length === 0 && (
           <>
-            <h1 className=" text-3xl font-semibold uppercase text-center w-full">
+            <h1 className=" text-3xl font-semibold uppercase text-center w-full mt-5">
               No jobs
             </h1>
           </>
         )}
-      </div>
+      </section>
     </div>
   );
 };
